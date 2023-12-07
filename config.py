7bff58141cbfbc88adeefff787e5e8d484559c95
Navaneetha516 <121594658+Navaneetha516@users.noreply.github.com>
2023-12-07 16:03:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "20053544"))
+API_HASH = environ.get("API_HASH", "f26c4d28081c11e0c48707143f7bb5b1")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6907922317:AAEHxAFg0QA2ShmtKbTNUWDTIo2mFAWO3_Q")
+ADMIN = int(environ.get("ADMIN", "1439022100"))          
+CAPTION = environ.get("CAPTION", "{filename} @Movie_Time_HD_1")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/52807f093e8b1f225a37a.jpg")
