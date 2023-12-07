db2179a6fd3a5d06705c2383e8f7b1c119184de5
Navaneetha516 <121594658+Navaneetha516@users.noreply.github.com>
2023-12-07 21:01:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20053544"))
 API_HASH = environ.get("API_HASH", "f26c4d28081c11e0c48707143f7bb5b1")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6907922317:AAEHxAFg0QA2ShmtKbTNUWDTIo2mFAWO3_Q")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6760219155:AAFjeqYvwckU8iXhUpfeb3Kx0yoxCUryHXc")
 ADMIN = int(environ.get("ADMIN", "1439022100"))          
-CAPTION = environ.get("CAPTION", "{filename} @Movie_Time_HD_1")
+CAPTION = environ.get("CAPTION", "0")
 
-class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/52807f093e8b1f225a37a.jpg")
+# for thumbnail ( back end is MrMKN brain 😉)
+DOWNLOAD_LOCATION = "./DOWNLOADS"
